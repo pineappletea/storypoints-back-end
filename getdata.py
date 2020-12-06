3e90257a997c0748110f001edbf103ef30672c78
pineappletea <47184314+pineappletea@users.noreply.github.com>
2020-12-06 17:46:12 +0200

    requirements

--- getdata.py
+++ getdata.py
@@ -1,43 +1,44 @@
+# -*- coding: utf-8 -*-
 def testdata():
 
     data = ({
-        "StoryTitle": "Testitarina yksi",
+        "StoryTitle": "Example story 1",
         "WeekOfCreation": "12/2020",
         "Coordinates": {
             "lat": 60.201534768722134,
             "long": 24.934345339859608
         },
-        "LocationDescription": "Keskellä aulaa",
+        "LocationDescription": "Stand in the middle of the lobby",
         "ThemeTags": ["studying", "work"],
         "UpVotes": "10",
     }, {
-        "StoryTitle": "Testitarina kaksi",
+        "StoryTitle": "Example story 2",
         "WeekOfCreation": "12/2020",
         "Coordinates": {
             "lat": 60.20307439963429,
             "long": 24.936014543166838
         },
-        "LocationDescription": "Käytävällä",
+        "LocationDescription": "In the hallway",
         "ThemeTags": ["studying", "work"],
         "UpVotes": "10"
     }, {
-        "StoryTitle": "Testitarina kolme",
+        "StoryTitle": "Example story 3",
         "WeekOfCreation": "12/2020",
         "Coordinates": {
             "lat": 60.19942584165076,
             "long": 24.93296435936728
         },
-        "LocationDescription": "Keskimmäiset penkit, laituri 3",
+        "LocationDescription": "Benches on platform 3",
         "ThemeTags": [],
         "UpVotes": "10"
     }, {
-        "StoryTitle": "Testitarina Espoossa",
+        "StoryTitle": "Example story in Espoo",
         "WeekOfCreation": "12/2020",
         "Coordinates": {
             "lat": 60.19695364485905,
             "long": 24.669034305277542
         },
-        "LocationDescription": "Kivellä",
+        "LocationDescription": "On a rock",
         "ThemeTags": [],
         "UpVotes": "10"
     }
